578507a0a1899c44760e60f525b7ff804231fe70
Rogerio Chaves <rogeriocfj@gmail.com>
2017-05-13 00:22:14 -0300

    Fix elm exporting

--- minify.sh
+++ minify.sh
@@ -3,6 +3,7 @@ set -e
 elm make Main.elm --output main.js
 sed -e "5248,5252d" -i.bak main.js # remove duplicated badIndex
 prepack main.js --out main.prepacked.js
+sed -e "s/Elm =/this['Elm'] =/" -i.bak main.prepacked.js # reexport Elm
 uglifyjs --compress unused,dead_code main.prepacked.js > main.prepacked.min.js
 gzip --stdout main.prepacked.min.js > main.prepacked.min.gz
 du -h main*
